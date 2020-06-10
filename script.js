a58b8be0dce28152d6c38c863de8c178f7615afa
cezary-gawronski <idetepowiec@gmail.com>
2020-06-10 13:54:54 +0200

    script is working :)

--- script.js
+++ script.js
@@ -13,46 +13,44 @@ fontColorInput.addEventListener("input", () => {
 
 
 
-
 const saveText = document.querySelector(".screen-color-input--js");
-// const entry = localStorage.getItem('entry');
-// let result = '';
-
-// if (entry) {
-//     result = entry;
-// }
-
 const entryInput = document.querySelector('.form__textarea--js');
-// entryInput.value = result;
-
 const saveButton = document.querySelector('.save-button--js');
 const loadButton = document.querySelector('.load-button--js');
 const clearButton = document.querySelector('.clear-button--js');
+entryInput.classList.add('do-not-save--js');
 
 saveButton.addEventListener('click', () => {
-    localStorage.setItem('entry', entryInput.value);
+    if ((entryInput.classList.contains('red-alert--js')) || (entryInput.classList.contains('do-not-save--js'))) {
+        entryInput.classList.add('red-alert--js');
+        entryInput.value = "KLIKNIJ i pisz lub ZAŁADUJ dane:)";
+    } else {
+        localStorage.setItem('entry', entryInput.value);
+    }
 });
 
 loadButton.addEventListener('click', () => {
+    entryInput.classList.add('do-not-save--js');
     result = localStorage.getItem('entry');
     if (result) {
         entryInput.value = result;
-        entryInput.classList.remove('red-alert');
+        entryInput.classList.remove('red-alert--js');
         entryInput.value = `Załadowano dane które zapisałeś podczas ostatniej wizyty: „${result}”`;
     } else {
         entryInput.value = "Brak danych do załadowania. Wpisz nowe dane i naciśnij ENTER!";
-        entryInput.classList.add('red-alert');
+        entryInput.classList.add('red-alert--js');
     }
 
 });
 
 clearButton.addEventListener('click', () => {
-    entryInput.value = 'Wyczyściłeś ekran. ZAŁADUJ lub DODAJ nowe dane.';
-    entryInput.classList.add('red-alert');
+    entryInput.value = 'Wyczyściłeś ekran. ZAŁADUJ ponownie dane lub DODAJ nowe.';
+    entryInput.classList.add('red-alert--js');
 });
 
 entryInput.addEventListener('click', () => {
-    entryInput.classList.remove('red-alert');
+    entryInput.classList.remove('do-not-save--js');
+    entryInput.classList.remove('red-alert--js');
     entryInput.value = '';
 
 });
